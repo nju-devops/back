66cc5efd7a1c86a1da21c8b8589b97ac70cc47d9
Fullcreammilk <852805936@qq.com>
2020-03-20 13:19:47 +0800

    找边逻辑更新

--- src/main/java/com/nju/aop/repository/ChainRepository.java
+++ src/main/java/com/nju/aop/repository/ChainRepository.java
@@ -3,6 +3,8 @@ package com.nju.aop.repository;
 
 import com.nju.aop.dataobject.Chain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +15,12 @@ public interface ChainRepository extends JpaRepository<Chain, Integer> {
 
     List<Chain> findByAopId(Integer aopId);
 
+    List<Chain> findByAopIdAndType(Integer aopId, String type);
+
     List<Chain> findByEventId(Integer eventId);
+
+    List<Chain> findByEventIdAndType(Integer eventId, String type);
+
+    @Query("select distinct c.eventId from Chain c where c.type=:type")
+    List<Integer> findEventIdByType(@Param("type") String type);
 }
